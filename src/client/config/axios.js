eb0eef3a73b2405f189bcfbb296793af7048a80d
Nikita Shchypylov <shchypylov@gmail.com>
2018-09-16 23:33:22 +0300

    upd

--- src/client/config/axios.js
+++ src/client/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: process.env.BASE_URL,
+	baseURL: process.env.BASE_URL || "/api",
 });
 
 axiosInstance.interceptors.response.use(res => {
