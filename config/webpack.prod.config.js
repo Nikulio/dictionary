6d0f977c6d2af19de8dbabf3c100d3b022ed5c75
Nikita Shchypylov <shchypylov@gmail.com>
2018-09-17 15:05:22 +0300

    testing

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,7 +5,8 @@ const baseConfig = require('./webpack.base.config');
 const optimizationConfig = require('./webpack.opt.config');
 
 const productionConfiguration = function (env) {
-	const NODE_ENV = 'production';
+	const {NODE_ENV} = process.env;
+	console.log("--- 1", NODE_ENV);
 	return {
 		plugins: [
 			new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(NODE_ENV)}),
